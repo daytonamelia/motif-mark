1c6463c12ac732f659d9c474713fa2ab85a58d20
Amelia Dayton <daytonamelia@gmail.com>
2025-02-26 13:47:09 -0800

    Drawing showing up, not to scale

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -18,6 +18,8 @@ ambig_nts = {
     "V": "[^T]",
     "N": "(A|T|C|G)"}
 
+EXON_HEIGHT = 10
+INTRON_HEIGHT = EXON_HEIGHT / 2
 
 # ARGUMENT/FILE PARSERS
 def get_args():
@@ -119,62 +121,82 @@ def gene_splitter(read: str) -> list:
 # CLASSES
 class Gene:
     '''A Gene object. A gene has an index, Features (introns and exons), and a length of the total gene.'''
-    def __init__(self, index, seq):
+    def __init__(self, index, seq) -> None:
         '''Pass an index (fasta header or name) and a sequence that is split into a list of Feature objects based on capitalization of the sequence.'''
         self.index = index
         self.length = int(len(seq))
         self.features = gene_splitter(seq)
     
-    def __str__(self):
+    def __str__(self) -> str:
         '''Print magic method. Returns just the index.'''
         return f"{self.index}"
     
-    def __len__(self):
+    def __len__(self) -> int:
         '''Length magic method. Returns length of the entire gene, introns and exons.'''
         return self.length
 
-    def count(self):
+    def count(self) -> int:
         '''Returns integer count of features (introns or exons).'''
         return len(self.features)
 
-    def draw(self, surface):
+    def draw_features(self, surface: cairo.Surface, surface_x: int, surface_y: int, x: int, y: int):
         '''Given a top-left point, draws a rectangle and line.'''
         print(self.length)
+        context = cairo.Context(surface)
+        context.set_line_width(0.1)
+        curr_x = x
+        curr_y = y
+        for feature in self.features:
+            if feature.is_exon():
+                # x,y,width,height 
+                context.rectangle(curr_x, curr_y, len(feature), curr_y + EXON_HEIGHT)
+                context.stroke()
+                curr_x = len(feature)
+            else:
+                # x,y -> x,y
+                context.move_to(curr_x, curr_y - INTRON_HEIGHT)
+                context.line_to(len(feature), curr_y - INTRON_HEIGHT)
+                context.stroke()
+                curr_x = len(feature)
+
+
+
 
 
 class Feature:
     '''A Feature object. A feature is either an exon (exon == True) or an intron (exon == False).'''
-    def __init__(self, seq, exon):
+    def __init__(self, seq, exon) -> None:
         self.seq = seq
         self.revcomp = revcomp(seq)
         self.length = int(len(seq))
         self.exon = exon
     
-    def __str__(self):
+    def __str__(self) -> str:
         '''Print magic method.'''
         return self.seq
 
-    def __len__(self):
+    def __len__(self) -> int:
         '''Length magic method.'''
         return self.length
 
-    def is_exon(self):
+    def is_exon(self) -> bool:
         '''Returns True if feature is an exon.'''
         return self.exon
 
+
 class Motif:
     '''A Motif object.'''
-    def __init__(self, motif):
+    def __init__(self, motif) -> None:
         '''From passing the motif string, finds attributes length and regex.'''
         self.motif = motif
         self.length = len(motif)
         self.regex = motif_regex(motif)
 
-    def __str__(self):
+    def __str__(self) -> str:
         '''Print magic method'''
         return f"{self.motif} with pattern {self.regex}"
 
-    def __len__(self):
+    def __len__(self) -> int:
         '''Length magic method.'''
         return self.length
 
@@ -187,15 +209,14 @@ testgene = records[0]
 print(testgene)
 
 # draw
-with cairo.PDFSurface("motif-mark.pdf", 1000, 1000) as surface:
+with cairo.PDFSurface("motif-mark.pdf", 100, 100) as surface:
     # Set context
     context = cairo.Context(surface)
-    context.scale(1000,1000)
-    context.set_line_width(0.01)
+    context.scale(100,100)
 
     # Make a rectangle for a gene
     print("TESTING DRAW:")
-    testgene.draw(surface)
+    testgene.draw_features(surface, 100, 100, 0, 50)
 
 
     
